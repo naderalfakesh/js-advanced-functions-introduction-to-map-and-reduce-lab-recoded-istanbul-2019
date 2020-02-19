c0a8cbdbd3a903cf1a9271b5b24546ff8f864be1
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 22:10:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function mapToNegativize(arr){
   }
   return result;
 }
+function mapToDouble(arr){
+  const result =[];
+  for(const elm of arr){
+    result.push(2*elm);
+  }
+  return result;
+}
 
 function mapToNoChange(arr){
   return [...arr];
